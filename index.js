3818fee5f5564419b82e575f8321049cb5702ab7
Gijs Nelissen <gijs@prezly.com>
2017-06-14 13:45:11 +0200

    Fixed wistia regex + loading wistia snippet

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ function getEmbeddedPlayer(url, height, width){
     var output = '';
     var youtubeUrl = url.match(/watch\?v=([a-zA-Z0-9\-_]+)/);
     var vimeoUrl = url.match(/^https?:\/\/(www\.)?vimeo\.com\/(clip\:)?(\d+).*$/);
-    var wistiaUrl = url.match(/^https?:\/\/(www\.)?wistia\.com\/(medias\:)?(\w+).*$/);
+    var wistiaUrl = url.match(/^https?:\/\/(?:(\w+)\.)?wistia\.com\/(medias\/)?(\w+)$/);
+
+
 
     var style = "border: none;position: absolute;top: 0;left: 0;width: " + width + ";height: " + height;
 
@@ -15,11 +17,22 @@ function getEmbeddedPlayer(url, height, width){
     }else if(vimeoUrl){
         output =  '<iframe style="' + style + '" src="https://player.vimeo.com/video/'+vimeoUrl[3]+'" frameborder="0"></iframe>';
     }else if (wistiaUrl) {
-        output =  '<script src="https://fast.wistia.com/embed/medias/3mki34ur34.jsonp" async></script><script src="https://fast.wistia.com/assets/external/E-v1.js" async></script><div class="wistia_responsive_padding" style="padding:82.19% 0 0 0;position:relative;"><div class="wistia_responsive_wrapper" style="height:100%;left:0;position:absolute;top:0;width:100%;"><div class="wistia_embed wistia_async_3mki34ur34 seo=false videoFoam=true" style="height:100%;width:100%">&nbsp;</div></div></div>';
+//div approach
+
+        output =  '<script src="https://fast.wistia.com/embed/medias/'+wistiaUrl[3]+'.jsonp" async></script>';
+        output += '<script src="https://fast.wistia.com/assets/external/E-v1.js" async></script>';
+        // output += '<div class="wistia_responsive_padding" style="padding:64.06% 0 0 0;position:relative;>';
+            output += '<div class="wistia_responsive_wrapper" style="height:100%;left:0;position:absolute;top:0;width:100%;">';
+                output += '<div class="wistia_embed wistia_async_'+wistiaUrl[3]+' seo=false videoFoam=true" style="height:100%;width:100%">&nbsp;</div>';
+            output += '</div>';
+        // output += '</div>';
+
+        //iframe
+        //output = '<iframe src="https://fast.wistia.net/embed/iframe/'+wistiaUrl[3]+'?seo=false&videoFoam=true" title="Wistia video player" allowtransparency="true" frameborder="0" scrolling="no" class="wistia_embed" name="wistia_embed" allowfullscreen mozallowfullscreen webkitallowfullscreen oallowfullscreen msallowfullscreen width="100%" height="100%"></iframe><script src="https://fast.wistia.net/assets/external/E-v1.js" async></script>';
     }else{
         output = '<p>No video url found - vimeo and youtube supported</p>';
     }
-    return '<div style="position: relative;padding-bottom: 56.25%;padding-top: 25px;height: 0;">' + output + '</div>';
+    return '<div style="position: relative;padding-bottom: 64%;padding-top: 0px;height: 0;">' + output + '</div>';
 }
 
 module.exports = {
